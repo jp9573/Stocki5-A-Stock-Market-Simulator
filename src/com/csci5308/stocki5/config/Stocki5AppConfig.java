8b82710583d23ea5168148147e5457532232b2e0
Vishal Dipak Parmar <vs301167@dal.ca>
2021-02-18 21:51:55 +0530

    Spring MVC initial project commit

---
+++ src/com/csci5308/stocki5/config/Stocki5AppConfig.java
@@ -0,0 +1,31 @@
+package com.csci5308.stocki5.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.csci5308.stocki5" })
+public class Stocki5AppConfig implements WebMvcConfigurer {
+
+	@Bean
+	public InternalResourceViewResolver resolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/hello").setViewName("helloworld");
+	}
+
+}
