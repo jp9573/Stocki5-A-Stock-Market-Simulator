a2626e4e21c472c6ae52e0edf7629827e370bf4a
Vishal Dipak Parmar <vs301167@dal.ca>
2021-02-20 19:52:17 +0530

    Changes in properties file and configuring pom.xml for picking properties file based on environment

--- src/main/java/com/csci5308/stocki5/controller/HelloWorldController.java
+++ src/main/java/com/csci5308/stocki5/controller/HelloWorldController.java
@@ -7,6 +7,9 @@ import java.sql.Statement;
 import java.time.LocalDateTime;
 
 import com.csci5308.stocki5.config.Stocki5DbConnection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,25 +19,25 @@ import com.csci5308.stocki5.model.HelloWorld;
 @Controller
 public class HelloWorldController {
 
+	@Autowired
+	Stocki5DbConnection stocki5DbConnection;
+
 	@RequestMapping("/helloworld")
 	public String handler(Model model) throws SQLException {
 		HelloWorld helloWorld = new HelloWorld();
 
-		Stocki5DbConnection con = new Stocki5DbConnection();
-		Connection c = con.createConnection();
+		Connection c = stocki5DbConnection.createConnection();
 		Statement statement = c.createStatement();
 		ResultSet resultSet = statement.executeQuery("SELECT * FROM demo WHERE id=1");
 		while (resultSet.next()) {
 			System.out.println(resultSet.getInt("id"));
 			System.out.println(resultSet.getString("name"));
 		}
-		con.closeConnection(c);
-
+		stocki5DbConnection.closeConnection(c);
 
 		helloWorld.setMessage("Hello World Example Using Spring MVC 5!!!");
 		helloWorld.setDateTime(LocalDateTime.now().toString());
 		model.addAttribute("helloWorld", helloWorld);
-
 
 		return "helloworld";
 
