35e26aff6018cbfdee37268a6f062d136060b0a7
Vishal Dipak Parmar <vs301167@dal.ca>
2021-02-25 19:07:33 +0530

    Add role in User.java, UserSignUp.java and UserDb.java

--- src/main/java/com/csci5308/stocki5/user/UserSignUp.java
+++ src/main/java/com/csci5308/stocki5/user/UserSignUp.java
@@ -7,6 +7,12 @@ public class UserSignUp
 {
 	public boolean addUser(UserDbInterface dbInterface, UserCode user)
 	{
+		user.setFunds(50000.0);
+		user.setInternationalCommodityExchange(1);
+		user.setInternationalDerivativeExchange(1);
+		user.setInternationalStockExchange(1);
+		user.setForeignExchange(1);
+		user.setRole("ROLE_USER");
 		user.generateUserCode();
 		return dbInterface.insertUser(user);
 	}
