f21c571f844a0628e8201fbf0ecc52715236b6c7
Vishal Dipak Parmar <vs301167@dal.ca>
2021-02-25 16:22:45 +0530

    UserSignUp service and test added along with mock db object to test functionality.

---
+++ src/main/java/com/csci5308/stocki5/user/UserDbInterface.java
@@ -0,0 +1,13 @@
+package com.csci5308.stocki5.user;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDbInterface
+{
+	public boolean insertUser(User user);
+
+	public boolean updateUser(User user);
+
+	public User getUser(String userCode);
+}
