a7b572aa341021f6a70eedd131dfb2e6fa030ce8
Vishal Dipak Parmar <vs301167@dal.ca>
2021-02-25 17:52:04 +0530

    Add junit tests for date of birth

--- src/test/java/com/csci5308/stocki5/user/UserTest.java
+++ src/test/java/com/csci5308/stocki5/user/UserTest.java
@@ -1,5 +1,8 @@
 package com.csci5308.stocki5.user;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -66,9 +69,9 @@ public class UserTest
 		user.setEmailId("example@example.com");
 		user.setContactNo("19324678786");
 		user.setFirstName("John");
-		user.setLastName("Do");
+		user.setLastName("Doe");
 		user.setProvince("ha");
-		Assert.assertEquals("Last name cannot be less than 3 characters or more than 20 characters.", user.validate());
+		Assert.assertEquals("State/Province cannot be less than 3 characters or more than 20 characters.", user.validate());
 	}
 	
 	@Test
@@ -77,9 +80,37 @@ public class UserTest
 		user.setEmailId("example@example.com");
 		user.setContactNo("19324678786");
 		user.setFirstName("John");
-		user.setLastName("Doxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+		user.setLastName("Doe");
 		user.setProvince("halifaxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
-		Assert.assertEquals("Last name cannot be less than 3 characters or more than 20 characters.", user.validate());
+		Assert.assertEquals("State/Province cannot be less than 3 characters or more than 20 characters.", user.validate());
+	}
+	
+	@Test
+	public void validateDateOfBirth() {
+		user.setEmailId("example@example.com");
+		user.setContactNo("19324678786");
+		user.setFirstName("John");
+		user.setLastName("Doe");
+		user.setProvince("halifax");
+		user.setDateOfBirth(new Date());
+		Assert.assertEquals("Invalid Date of Birth.", user.validate());
+	}
+	
+	@Test
+	public void validateDateOfBirthMoreThanEighteenYears() {
+		user.setEmailId("example@example.com");
+		user.setContactNo("19324678786");
+		user.setFirstName("John");
+		user.setLastName("Doe");
+		user.setProvince("halifax");
+		
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.YEAR, -17);
+		cal.add(Calendar.DATE, -1);
+		Date date = cal.getTime();
+		
+		user.setDateOfBirth(date);
+		Assert.assertEquals("Minimum age to sign up with STOCKI5 is 18 years or above.", user.validate());
 	}
 	
 	@Test
@@ -90,6 +121,13 @@ public class UserTest
 		user.setFirstName("John");
 		user.setLastName("Doe");
 		user.setProvince("halifax");
+		
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.YEAR, -18);
+		cal.add(Calendar.DATE, -1);
+		Date date = cal.getTime();
+		
+		user.setDateOfBirth(date);
 		user.setPassword("1234567");
 		Assert.assertEquals("Password cannot be less than 8 characters or more than 20 characters.", user.validate());
 	}
@@ -102,6 +140,13 @@ public class UserTest
 		user.setFirstName("John");
 		user.setLastName("Doe");
 		user.setProvince("halifax");
+		
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.YEAR, -18);
+		cal.add(Calendar.DATE, -1);
+		Date date = cal.getTime();
+		
+		user.setDateOfBirth(date);
 		user.setPassword("1234567123334135788eus2rdf3");
 		Assert.assertEquals("Password cannot be less than 8 characters or more than 20 characters.", user.validate());
 	}
@@ -114,6 +159,13 @@ public class UserTest
 		user.setFirstName("John");
 		user.setLastName("Doe");
 		user.setProvince("halifax");
+		
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.YEAR, -18);
+		cal.add(Calendar.DATE, -1);
+		Date date = cal.getTime();
+		
+		user.setDateOfBirth(date);
 		user.setPassword("12345678");
 		user.setConfirmPassword("123456789");
 		Assert.assertEquals("Password and confirm password missmatch.", user.validate());
@@ -127,6 +179,13 @@ public class UserTest
 		user.setFirstName("John");
 		user.setLastName("Doe");
 		user.setProvince("halifax");
+		
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.YEAR, -18);
+		cal.add(Calendar.DATE, -1);
+		Date date = cal.getTime();
+		
+		user.setDateOfBirth(date);
 		user.setPassword("12345678");
 		user.setConfirmPassword("12345678");
 		Assert.assertEquals("valid", user.validate());
