2646b3ff5de0900b5b9809c1e8da92b17bd0f1ac
Vishal Dipak Parmar <vs301167@dal.ca>
2021-02-25 15:41:58 +0530

    add UserCode.java and UserCodeTest.java for generating and testing user code

---
+++ src/main/java/com/csci5308/stocki5/user/UserCode.java
@@ -0,0 +1,21 @@
+package com.csci5308.stocki5.user;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UserCode extends User
+{
+	public String generateUserCode()
+	{
+		if (null != super.getFirstName() && null != super.getLastName())
+		{
+			char firstNameFirstChar = super.getFirstName().charAt(0);
+			char lastNameFirstChar = super.getLastName().charAt(0);
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+			String date = simpleDateFormat.format(new Date());
+			String userCode = String.valueOf(firstNameFirstChar) + String.valueOf(lastNameFirstChar) + date;
+			return userCode.toUpperCase();
+		}
+		return null;
+	}
+}
