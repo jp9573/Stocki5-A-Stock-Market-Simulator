316b237ccaff5d4a7214b8fa3e8f2cb4d8452a5c
JOHANAN ABHISHEK PRABHURAI <jh416787@dal.ca>
2021-02-27 00:49:20 +0530

    Adding featrure forgot usercode

---
+++ src/main/java/com/csci5308/stocki5/user/UserForgotCode.java
@@ -0,0 +1,29 @@
+package com.csci5308.stocki5.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service
+public class UserForgotCode implements UserForgotCodeInterface {
+
+    public String getUserCode(String email, String dob, UserDbInterface userDb){
+        String result = null;
+        User user = userDb.getUserByEmail(email);
+        if(user != null) {
+            try {
+                Date dobDate = new SimpleDateFormat("yyyy-MM-dd").parse(dob);
+                if (user.getEmailId().equals(email) && user.getDateOfBirth().compareTo(dobDate) == 0) {
+                    result = user.getUserCode();
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+                return result;
+            }
+        }
+        return result;
+    }
+}
--- src/test/java/com/csci5308/stocki5/user/UserDbMock.java
+++ src/test/java/com/csci5308/stocki5/user/UserDbMock.java
@@ -1,5 +1,9 @@
 package com.csci5308.stocki5.user;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class UserDbMock implements UserDbInterface
 {
 
@@ -22,6 +26,28 @@ public class UserDbMock implements UserDbInterface
 	{
 		System.out.println(userCode);
 		return null;
+	}
+
+	@Override
+	public User getUserByEmail(String email)
+	{
+		System.out.println(email);
+		if(email.equals("test@example.com")) {
+			User user = new User();
+			user.setEmailId(email);
+			user.setUserCode("TEST12456789");
+			Date dob = null;
+			try {
+				dob = new SimpleDateFormat("yyyy-MM-dd").parse("2000-10-12");
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			user.setDateOfBirth(dob);
+			return user;
+		}
+		else {
+			return null;
+		}
 	}
 
 }
