414d4bf82638361471d60b6ec633bf0da4087381
Vishal Dipak Parmar <vs301167@dal.ca>
2021-03-06 09:35:41 +0530

    add percent increase decrease for stock price

--- src/test/java/com/csci5308/stocki5/stock/StockPriceAlogrithmTest.java
+++ src/test/java/com/csci5308/stocki5/stock/StockPriceAlogrithmTest.java
@@ -11,14 +11,18 @@ public class StockPriceAlogrithmTest {
 
 	private float currentPrice = 0.00f;
 
+	private float previousPrice = 0.00f;
+
 	@Before
 	public void createStock() {
 		currentPrice = 125.02f;
+		previousPrice = 42.63f;
 	}
 
 	@After
 	public void destroyStock() {
 		currentPrice = 0.00f;
+		previousPrice = 0.00f;
 	}
 
 	@Test
@@ -39,21 +43,28 @@ public class StockPriceAlogrithmTest {
 		float newStockPrice = stockPriceAlgorithm.stockPriceAlgorithm(currentPrice);
 		Assert.assertTrue(minStockPrice <= newStockPrice);
 	}
-	
+
 	@Test
 	public void stockPriceAlgorithmTestNegativeNumber() {
 		currentPrice = -25.00f;
 		float newStockPrice = stockPriceAlgorithm.stockPriceAlgorithm(currentPrice);
 		Assert.assertTrue(newStockPrice > 0.00f);
 	}
-	
+
 	@Test
 	public void stockPriceAlgorithmTestZero() {
 		currentPrice = 0.00f;
 		float newStockPrice = stockPriceAlgorithm.stockPriceAlgorithm(currentPrice);
 		Assert.assertTrue(newStockPrice > 0.00f);
 	}
-	
+
+	@Test
+	public void stockPricePercentIncreaseDecreaseTest() {
+		currentPrice = 47.89f;
+		float percent = stockPriceAlgorithm.stockPricePercentIncreaseDecrease(currentPrice, previousPrice);
+		Assert.assertTrue(percent == 12.34f);
+	}
+
 	@Test
 	public void generateStockPriceTest() {
 		Assert.assertTrue(true);
