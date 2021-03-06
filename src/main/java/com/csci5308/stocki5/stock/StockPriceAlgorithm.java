31cc735a4bdaa854ea1bd05ea320e23b0d142cf1
Vishal Dipak Parmar <vs301167@dal.ca>
2021-03-06 09:35:41 +0530

    add percent increase decrease for stock price

--- src/main/java/com/csci5308/stocki5/stock/StockPriceAlgorithm.java
+++ src/main/java/com/csci5308/stocki5/stock/StockPriceAlgorithm.java
@@ -29,15 +29,28 @@ public class StockPriceAlgorithm {
 		Random random = new Random();
 		float maxPriceRange = currentPrice + 10;
 		float minPriceRange = currentPrice - 10;
-		if (minPriceRange < 0.0f) {
+		if (minPriceRange < 0.00f) {
 			minPriceRange = 0.00f;
 		}
-		if (maxPriceRange < 0.0f) {
+		if (maxPriceRange < 0.00f) {
 			maxPriceRange = 1.00f;
 		}
 		float newPrice = minPriceRange + random.nextFloat() * (maxPriceRange - minPriceRange);
 		float formatedNewPrice = Float.parseFloat(df.format(newPrice));
 		return formatedNewPrice;
+	}
+
+	public float stockPricePercentIncreaseDecrease(float currentPrice, float previousClose) {
+		DecimalFormat df = new DecimalFormat("##.00");
+		float percent = 0.00f;
+		float formatedPercent = 0.00f;
+		if (previousClose <= 0.00f) {
+			formatedPercent = Float.parseFloat(df.format(percent));
+		} else {
+			percent = ((currentPrice - previousClose) / previousClose) * 100;
+			formatedPercent = Float.parseFloat(df.format(percent));
+		}
+		return formatedPercent;
 	}
 
 }
