bdc11ba9abc42eccaf6fdeee232eac4f1d846bf2
Jay Patel <jp9573@gmail.com>
2021-03-07 20:25:03 +0530

    Add top gainers and losers

--- src/main/java/com/csci5308/stocki5/stock/StockDbInterface.java
+++ src/main/java/com/csci5308/stocki5/stock/StockDbInterface.java
@@ -8,13 +8,17 @@ import org.springframework.stereotype.Repository;
 public interface StockDbInterface
 {
 	public List<Stock> getStocks();
-	
+
 	public List<Stock> getStocksBySegment(String segments);
-	
+
+	public List<Stock> getHighestPriceStocks(String segments, int limit);
+
+	public List<Stock> getLowestPriceStocks(String segments, int limit);
+
 	public Stock getStockData(int stockId);
-	
+
 	public boolean updateStockData(Stock stock);
-	
+
 	public boolean updateStockBulk(List<Stock> stocks);
-	
+
 }
