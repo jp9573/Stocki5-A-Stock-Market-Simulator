fcc695c35b4be956fb54d51334cfe290984ba74f
Kirtan Dudhatra <kirtandudhatra@Kirtans-MacBook-Air.local>
2021-03-08 02:20:32 +0530

    test autocorrect extension fixed

--- src/test/java/com/csci5308/stocki5/stock/StockDataUpdateTest.java
+++ src/test/java/com/csci5308/stocki5/stock/StockDataUpdateTest.java
@@ -1,10 +1,9 @@
 package com.csci5308.stocki5.stock;
 
-import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class StockDataUpdateTest extends TestCase {
+public class StockDataUpdateTest {
 
     StockDbInterface dbInterface = new StockDbMock();
     StockDataUpdate stock = new StockDataUpdate(1, dbInterface);
