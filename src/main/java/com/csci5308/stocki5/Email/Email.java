9e5a4bb2a4f98d7c4fc25c8c9f8c1a3f8630ed49
JOHANAN ABHISHEK PRABHURAI <jh416787@dal.ca>
2021-03-10 21:10:28 +0530

    Added email mock

--- src/main/java/com/csci5308/stocki5/Email/Email.java
+++ src/main/java/com/csci5308/stocki5/Email/Email.java
@@ -4,7 +4,6 @@ import com.csci5308.stocki5.config.Stocki5AppEmailConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 @Service
