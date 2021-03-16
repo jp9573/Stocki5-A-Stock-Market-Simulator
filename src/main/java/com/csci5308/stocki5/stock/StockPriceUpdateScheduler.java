c3f27c9aa8adc2bad84e1abf4c59d32592fa1e0c
JOHANAN ABHISHEK PRABHURAI <jh416787@dal.ca>
2021-03-16 15:38:04 +0530

    reverted fixedDelay back to 5 mins

--- src/main/java/com/csci5308/stocki5/stock/StockPriceUpdateScheduler.java
+++ src/main/java/com/csci5308/stocki5/stock/StockPriceUpdateScheduler.java
@@ -12,7 +12,7 @@ public class StockPriceUpdateScheduler extends StockPriceAlgorithm {
 	@Autowired
 	StockDb stockDb;
 
-	@Scheduled(fixedDelay = 2000)
+	@Scheduled(fixedDelay = 5000)
 	public void scheduleGenerateStockPrice() {
 		super.generateStockPrice(stockDb);
 	}
