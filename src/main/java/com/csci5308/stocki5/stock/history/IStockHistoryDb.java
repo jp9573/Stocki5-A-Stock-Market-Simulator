8e2b190743df240a57e5f444a4d9d0ee5e07b1b4
Vishal Dipak Parmar <vs301167@dal.ca>
2021-03-28 22:18:36 +0530

    Rename Interface for stock db history

--- src/main/java/com/csci5308/stocki5/stock/history/IStockHistoryDb.java
+++ src/main/java/com/csci5308/stocki5/stock/history/IStockHistoryDb.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface StockHistoryDbInterface
+public interface IStockHistoryDb
 {
 	public List<StockHistory> getStockHistory(String stockSymbol);
 
