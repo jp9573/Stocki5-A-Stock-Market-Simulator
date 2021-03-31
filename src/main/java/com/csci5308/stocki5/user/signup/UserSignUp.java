3f698dfd658b64f48bd8ab6079bb4b36b9e101fd
Jay Patel <jp9573@gmail.com>
2021-03-31 14:12:05 +0530

    Refactor UserSignUp & IUserSignUp

--- src/main/java/com/csci5308/stocki5/user/signup/UserSignUp.java
+++ src/main/java/com/csci5308/stocki5/user/signup/UserSignUp.java
@@ -5,33 +5,33 @@ import com.csci5308.stocki5.user.IUserDb;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserSignUp implements IUserSignUp
-{
+public class UserSignUp implements IUserSignUp {
+    private static final double INITIAL_FUNDS = 50000.0;
+    private static final int ONE_VALUE = 1;
+    private static final String USER_ROLE = "ROLE_USER";
+    private static final String ADD_USER_ERROR_MESSAGE = "Error in adding user. Please try again later.";
 
-	public boolean addUser(IUserDb dbInterface, User user, String dob)
-	{
-		boolean isDobValid = user.validateDateOfBirth(dob);
-		boolean isPasswordValid = user.validatePassword();
-		boolean isUserValid = user.validate();
 
-		if (isDobValid && isPasswordValid && isUserValid)
-		{
-			user.setFunds(50000.0);
-			user.setInternationalCommodityExchange(1);
-			user.setInternationalDerivativeExchange(1);
-			user.setInternationalStockExchange(1);
-			user.setForeignExchange(1);
-			user.setRole("ROLE_USER");
-			user.generateUserCode();
-			boolean isUserAdded = dbInterface.insertUser(user);
-			if (isUserAdded)
-			{
-				return true;
-			}
-			user.setValidityMessage("Error in adding user. Please try again later.");
-		}
-		return false;
+    public boolean addUser(IUserDb dbInterface, User user, String dob) {
+        boolean isDobValid = user.validateDateOfBirth(dob);
+        boolean isPasswordValid = user.validatePassword();
+        boolean isUserValid = user.validate();
 
-	}
-
+        if (isDobValid && isPasswordValid && isUserValid) {
+            user.setFunds(INITIAL_FUNDS);
+            user.setInternationalCommodityExchange(ONE_VALUE);
+            user.setInternationalDerivativeExchange(ONE_VALUE);
+            user.setInternationalStockExchange(ONE_VALUE);
+            user.setForeignExchange(ONE_VALUE);
+            user.setRole(USER_ROLE);
+            user.generateUserCode();
+            boolean isUserAdded = dbInterface.insertUser(user);
+            if (isUserAdded) {
+                return true;
+            } else {
+                user.setValidityMessage(ADD_USER_ERROR_MESSAGE);
+            }
+        }
+        return false;
+    }
 }
