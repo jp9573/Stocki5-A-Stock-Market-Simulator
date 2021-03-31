eb32f3856398c99b875fdfec6c682f976f6c127d
Jay Patel <jy680475@dal.ca>
2021-03-31 16:32:27 +0530

    Refactor UserSignUpController

--- src/main/java/com/csci5308/stocki5/user/signup/UserSignUpController.java
+++ src/main/java/com/csci5308/stocki5/user/signup/UserSignUpController.java
@@ -11,19 +11,19 @@ import com.csci5308.stocki5.user.UserDb;
 
 @Controller
 public class UserSignUpController {
-    public static final String FIRST_NAME = "firstName";
-    public static final String LAST_NAME = "lastName";
-    public static final String EMAIL_ID = "emailId";
-    public static final String CONTACT_NUMBER = "contactNumber";
-    public static final String ADDRESS = "address";
-    public static final String PROVINCE = "province";
-    public static final String COUNTRY = "country";
-    public static final String PASSWORD = "password";
-    public static final String CONFIRM_PASSWORD = "confirmPassword";
-    public static final String DOB = "dob";
-    public static final String GENDER = "gender";
-    public static final String ERROR = "error";
-    public static final String USERNAME = "username";
+    private static final String FIRST_NAME = "firstName";
+    private static final String LAST_NAME = "lastName";
+    private static final String EMAIL_ID = "emailId";
+    private static final String CONTACT_NUMBER = "contactNumber";
+    private static final String ADDRESS = "address";
+    private static final String PROVINCE = "province";
+    private static final String COUNTRY = "country";
+    private static final String PASSWORD = "password";
+    private static final String CONFIRM_PASSWORD = "confirmPassword";
+    private static final String DOB = "dob";
+    private static final String GENDER = "gender";
+    private static final String ERROR = "error";
+    private static final String USERNAME = "username";
 
     @Autowired
     UserDb userDb;
