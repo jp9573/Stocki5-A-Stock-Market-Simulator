3290f731dfb6d5375fd312a0c00620d3ca37c90e
Jay Patel <jp9573@gmail.com>
2021-03-31 16:46:03 +0530

    Refactor IUserDb

--- src/main/java/com/csci5308/stocki5/user/IUserDb.java
+++ src/main/java/com/csci5308/stocki5/user/IUserDb.java
@@ -4,24 +4,24 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IUserDb {
-    String userTable = "user";
-    String firstNameColumn = "firstName";
-    String lastNameColumn = "lastName";
-    String passwordColumn = "password";
-    String confirmPasswordColumn = "confirmPassword";
-    String emailIdColumn = "emailId";
-    String contactNoColumn = "contactNo";
-    String dateOfBirthColumn = "dateOfBirth";
-    String genderColumn = "gender";
-    String addressColumn = "address";
-    String provinceColumn = "province";
-    String countryColumn = "country";
-    String internationalStockExchangeColumn = "internationalStockExchange";
-    String internationalDerivativeExchangeColumn = "internationalDerivativeExchange";
-    String internationalCommodityExchangeColumn = "internationalCommodityExchange";
-    String foreignExchangeColumn = "foreignExchange";
-    String userCodeColumn = "userCode";
-    String fundsColumn = "funds";
+    String USER_TABLE = "user";
+    String FIRST_NAME_COLUMN = "firstName";
+    String LAST_NAME_COLUMN = "lastName";
+    String PASSWORD_COLUMN = "password";
+    String CONFIRM_PASSWORD_COLUMN = "confirmPassword";
+    String EMAIL_ID_COLUMN = "emailId";
+    String CONTACT_NO_COLUMN = "contactNo";
+    String DATE_OF_BIRTH_COLUMN = "dateOfBirth";
+    String GENDER_COLUMN = "gender";
+    String ADDRESS_COLUMN = "address";
+    String PROVINCE_COLUMN = "province";
+    String COUNTRY_COLUMN = "country";
+    String INTERNATIONAL_STOCK_EXCHANGE_COLUMN = "internationalStockExchange";
+    String INTERNATIONAL_DERIVATIVE_EXCHANGE_COLUMN = "internationalDerivativeExchange";
+    String INTERNATIONAL_COMMODITY_EXCHANGE_COLUMN = "internationalCommodityExchange";
+    String FOREIGN_EXCHANGE_COLUMN = "foreignExchange";
+    String USER_CODE_COLUMN = "userCode";
+    String FUNDS_COLUMN = "funds";
 
     boolean insertUser(User user);
 
@@ -36,5 +36,4 @@ public interface IUserDb {
     double getUserFunds(String userCode);
 
     boolean updateUserFunds(String userCode, double amount);
-
 }
