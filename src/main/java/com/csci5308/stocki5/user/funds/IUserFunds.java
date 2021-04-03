46669fbdbdd97a7400e41adfb434b95cdacca906
Jay Patel <jp9573@gmail.com>
2021-04-03 10:44:59 +0530

    Edit funds

--- src/main/java/com/csci5308/stocki5/user/funds/IUserFunds.java
+++ src/main/java/com/csci5308/stocki5/user/funds/IUserFunds.java
@@ -6,10 +6,8 @@ import com.csci5308.stocki5.user.db.IUserDb;
 import com.csci5308.stocki5.user.IUser;
 
 @Service
-public interface IUserFunds
-{
-	public boolean resetFunds(IUser user, IUserDb userDb);
+public interface IUserFunds {
+    public boolean resetFunds(IUser user, IUserDb userDb);
 
-	public void setResetFundAmount(int resetFundAmount);
-
+    public void setResetFundAmount(int resetFundAmount);
 }
