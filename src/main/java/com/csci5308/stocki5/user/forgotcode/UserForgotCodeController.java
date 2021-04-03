0be120705767a772aa900ad62530e75a65de9408
Jay Patel <jp9573@gmail.com>
2021-04-03 10:44:23 +0530

    Edit forgotcode

--- src/main/java/com/csci5308/stocki5/user/forgotcode/UserForgotCodeController.java
+++ src/main/java/com/csci5308/stocki5/user/forgotcode/UserForgotCodeController.java
@@ -9,44 +9,38 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class UserForgotCodeController
-{
-	public static final String FORGOT_USER_VIEW_NAME = "forgotuser";
-	public static final String EMAIL_ID = "emailId";
-	public static final String DOB = "dob";
-	public static final String ERROR_MESSAGE = "Invalid email or date of birth.";
-	public static final String SUCCESS_MESSAGE = "Your UserCode  is - ";
+public class UserForgotCodeController {
+    public static final String FORGOT_USER_VIEW_NAME = "forgotuser";
+    public static final String EMAIL_ID = "emailId";
+    public static final String DOB = "dob";
+    public static final String ERROR_MESSAGE = "Invalid email or date of birth.";
+    public static final String SUCCESS_MESSAGE = "Your UserCode  is - ";
 
-	@Autowired
-	UserDb userDb;
+    @Autowired
+    UserDb userDb;
 
-	@Autowired
-	IUserForgotCode iUserForgotCode;
+    @Autowired
+    IUserForgotCode iUserForgotCode;
 
-	@RequestMapping(value = "/forgotuser", method = RequestMethod.POST)
-	public ModelAndView userForgotCode(@RequestParam(value = EMAIL_ID, required = true) String email,
-			@RequestParam(value = DOB, required = true) String dob)
-	{
-		ModelAndView model = new ModelAndView();
-		model.setViewName("forgotuser");
-		String result = iUserForgotCode.getUserCode(email, dob, userDb);
-		if (null == result)
-		{
-			model.addObject("error", ERROR_MESSAGE);
-			return model;
-		} else
-		{
-			model.addObject("success", SUCCESS_MESSAGE + result);
-		}
-		return model;
-	}
+    @RequestMapping(value = "/forgotuser", method = RequestMethod.POST)
+    public ModelAndView userForgotCode(@RequestParam(value = EMAIL_ID, required = true) String email,
+                                       @RequestParam(value = DOB, required = true) String dob) {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("forgotuser");
+        String result = iUserForgotCode.getUserCode(email, dob, userDb);
+        if (null == result) {
+            model.addObject("error", ERROR_MESSAGE);
+            return model;
+        } else {
+            model.addObject("success", SUCCESS_MESSAGE + result);
+        }
+        return model;
+    }
 
-	@RequestMapping(value = "/forgotuser", method = RequestMethod.GET)
-	public ModelAndView userForgotCode()
-	{
-		ModelAndView model = new ModelAndView();
-		model.setViewName("forgotuser");
-		return model;
-	}
-
+    @RequestMapping(value = "/forgotuser", method = RequestMethod.GET)
+    public ModelAndView userForgotCode() {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("forgotuser");
+        return model;
+    }
 }
