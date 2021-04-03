a0c1e0fa4136f659dfa514a7b0acad08a3e3bb4b
Jay Patel <jy680475@dal.ca>
2021-04-03 15:34:09 +0530

    Edit user mock factory

--- src/test/java/com/csci5308/stocki5/user/factory/UserFactoryMock.java
+++ src/test/java/com/csci5308/stocki5/user/factory/UserFactoryMock.java
@@ -4,8 +4,15 @@ import com.csci5308.stocki5.user.IUser;
 import com.csci5308.stocki5.user.User;
 import com.csci5308.stocki5.user.db.IUserDb;
 import com.csci5308.stocki5.user.db.UserDbMock;
-import com.csci5308.stocki5.user.password.IUserOtp;
-import com.csci5308.stocki5.user.password.UserOtp;
+import com.csci5308.stocki5.user.forgotcode.IUserForgotCode;
+import com.csci5308.stocki5.user.forgotcode.UserForgotCode;
+import com.csci5308.stocki5.user.funds.IUserFunds;
+import com.csci5308.stocki5.user.funds.UserFunds;
+import com.csci5308.stocki5.user.password.*;
+import com.csci5308.stocki5.user.profile.IUserProfile;
+import com.csci5308.stocki5.user.profile.UserProfile;
+import com.csci5308.stocki5.user.signup.IUserSignUp;
+import com.csci5308.stocki5.user.signup.UserSignUp;
 
 public class UserFactoryMock extends UserAbstractFactoryMock {
     private static UserFactoryMock uniqueInstance = null;
@@ -28,7 +35,42 @@ public class UserFactoryMock extends UserAbstractFactoryMock {
     }
 
     @Override
+    public IUserProfile createUserProfile() {
+        return new UserProfile();
+    }
+
+    @Override
     public IUserDb createUserDbMock() {
         return new UserDbMock();
+    }
+
+    @Override
+    public IUserFunds createUserFunds() {
+        return new UserFunds();
+    }
+
+    @Override
+    public IUserSignUp createUserSignUp() {
+        return new UserSignUp();
+    }
+
+    @Override
+    public IUserForgotCode createUserForgotCode() {
+        return new UserForgotCode();
+    }
+
+    @Override
+    public IUserChangePassword createUserChangePassword() {
+        return new UserChangePassword();
+    }
+
+    @Override
+    public IUserForgotPassword createUserForgotPassword() {
+        return new UserForgotPassword();
+    }
+
+    @Override
+    public IUserOtpDb createUserOtpDbMock() {
+        return new UserOtpDbMock();
     }
 }
