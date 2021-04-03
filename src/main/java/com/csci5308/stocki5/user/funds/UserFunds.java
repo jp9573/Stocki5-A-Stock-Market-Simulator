1767bd637f9e7a0c8dafdcab3bf0db20313b04bd
Jay Patel <jy680475@dal.ca>
2021-04-03 10:44:59 +0530

    Edit funds

--- src/main/java/com/csci5308/stocki5/user/funds/UserFunds.java
+++ src/main/java/com/csci5308/stocki5/user/funds/UserFunds.java
@@ -6,27 +6,22 @@ import com.csci5308.stocki5.user.db.IUserDb;
 import com.csci5308.stocki5.user.IUser;
 
 @Service
-public class UserFunds implements IUserFunds
-{
-	@Value("${user.resetfundamount}")
-	private int resetFundAmount;
+public class UserFunds implements IUserFunds {
+    @Value("${user.resetfundamount}")
+    private int resetFundAmount;
 
+    @Override
+    public void setResetFundAmount(int resetFundAmount) {
+        this.resetFundAmount = resetFundAmount;
+    }
 
-	@Override
-	public void setResetFundAmount(int resetFundAmount) {
-		this.resetFundAmount = resetFundAmount;
-	}
-
-	@Override
-	public boolean resetFunds(IUser user, IUserDb userDb)
-	{
-		if (user.getFunds() < resetFundAmount)
-		{
-			user.setFunds(resetFundAmount);
-			return userDb.updateUserFunds(user.getUserCode(), resetFundAmount);
-		} else
-		{
-			return false;
-		}
-	}
+    @Override
+    public boolean resetFunds(IUser user, IUserDb userDb) {
+        if (user.getFunds() < resetFundAmount) {
+            user.setFunds(resetFundAmount);
+            return userDb.updateUserFunds(user.getUserCode(), resetFundAmount);
+        } else {
+            return false;
+        }
+    }
 }
