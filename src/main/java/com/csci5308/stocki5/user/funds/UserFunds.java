e616253518208d2c43c9137d245115e3f5b8d0a7
JOHANAN ABHISHEK PRABHURAI <jh416787@dal.ca>
2021-04-04 15:02:59 +0530

    Updating UserFunds to read properites file

--- src/main/java/com/csci5308/stocki5/user/funds/UserFunds.java
+++ src/main/java/com/csci5308/stocki5/user/funds/UserFunds.java
@@ -5,10 +5,51 @@ import com.csci5308.stocki5.user.db.IUserDb;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.DoubleSummaryStatistics;
+import java.util.Properties;
+
 @Service
 public class UserFunds implements IUserFunds {
-    @Value("${user.resetfundamount}")
+    private static final String PROPERTIES_FILE="config.properties";
+    
     private int resetFundAmount;
+
+    public UserFunds(){
+        readProperties();
+    }
+
+    private void readProperties()
+    {
+        InputStream inputStream = null;
+        try
+        {
+            Properties prop = new Properties();
+            inputStream = getClass().getClassLoader().getResourceAsStream(PROPERTIES_FILE);
+            if (inputStream == null)
+            {
+                throw new FileNotFoundException();
+            } else
+            {
+                prop.load(inputStream);
+            }
+            this.resetFundAmount = Integer.parseInt(prop.getProperty("user.resetfundamount"));
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        } finally
+        {
+            try
+            {
+                inputStream.close();
+            } catch (IOException ioe)
+            {
+                ioe.printStackTrace();
+            }
+        }
+    }
 
     @Override
     public void setResetFundAmount(int resetFundAmount) {
